76e779017c62fe783a01b33d5942dc8297fb5c70
CristianRG02 <cristianrojasg2002@gmail.com>
2022-12-19 11:41:45 -0500

    Commit 05 - login

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from config import config
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 #Models
 from models.ModelUser import ModelUser
@@ -11,10 +12,15 @@ from models.entities.User import User
 
 app=Flask(__name__)
 db=MySQL(app)
+login_manager_app=LoginManager(app)
 
 @app.route('/')
 def index():
     return redirect(url_for('login'))
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -25,6 +31,7 @@ def login():
         logged_user = ModelUser.login(db, user)
         if logged_user != None:
             if logged_user.password:
+                login_user(logged_user)
                 return redirect(url_for('home'))
             else:
                 flash("Invalid password ...")
@@ -35,10 +42,28 @@ def login():
     else:
         return render_template('auth/login.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1>Esta es una vista protegida</h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>", 404
+
 if __name__=='__main__':
     app.config.from_object(config['development'])
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_404)
     app.run()
--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -17,3 +17,19 @@ class ModelUser():
 
         except Exception as ex:
             raise Exception(ex)
+    
+    @classmethod
+    def get_by_id(self, db, id):
+        try:
+            cursor=db.connection.cursor()
+            sql="SELECT id, username, fullname FROM user WHERE id = '{}'".format(id)
+            cursor.execute(sql)
+            row=cursor.fetchone()
+            if row != None:
+                user=User(row[0], row[1], None, row[2])
+                return user
+            else:
+                return None
+
+        except Exception as ex:
+            raise Exception(ex)
